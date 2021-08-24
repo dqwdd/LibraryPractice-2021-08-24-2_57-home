0b9f651f76e1f6a9db1f15da0ec27dbce9bfe864
dqwdd <dqwdd@naver.com>
2021-08-24 15:50:04 +0900

    사진 크게 보기 화면 진입 - 액션바 제거

--- app/src/main/java/com/example/librarypractice_2021_08_24_2_57_home/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_2021_08_24_2_57_home/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.librarypractice_2021_08_24_2_57_home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -17,6 +19,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        profileImg.setOnClickListener {
+
+//            프로필 사진 클릭 이벤트 => 사진 크게 보는 화면으로 이동시키기
+            val Intent myIntent = Intent(mContext, View)
+            startActivity(myIntent)
+//            매니페스트가서 노 액션바로 더 프로필보는것처럼 보이게
+
+        }
     }
 
     override fun setValues() {
---
+++ app/src/main/java/com/example/librarypractice_2021_08_24_2_57_home/ViewPhotoActivity.kt
@@ -0,0 +1,24 @@
+package com.example.librarypractice_2021_08_24_2_57_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
