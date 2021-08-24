b7ee2fe50038c6d406365eafe4befe1decfc50ff
dqwdd <dqwdd@naver.com>
2021-08-24 15:16:26 +0900

    액티비티 onCreate 정리용 클래스인 BaseActivity.kt 추가 함

--- app/src/main/java/com/example/librarypractice_2021_08_24_2_57_home/BaseActivity.kt
+++ app/src/main/java/com/example/librarypractice_2021_08_24_2_57_home/BaseActivity.kt
@@ -1,4 +1,17 @@
 package com.example.librarypractice_2021_08_24_2_57_home
 
-class BaseActivity {
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+//    this 자리에 대신 들어갈 변수를 미리 만들어두자
+    val mContext = this
+
+//    이벤트 처리 관련 코드를 모아줄 함수
+    abstract fun setupEvents()
+
+//    값을 띄우는 함수를 모아줄 코드
+    abstract fun setValues()
+
+
 }
--- app/src/main/java/com/example/librarypractice_2021_08_24_2_57_home/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_2021_08_24_2_57_home/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.librarypractice_2021_08_24_2_57_home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(mContext, "ss", Toast.LENGTH_SHORT).show()
+
     }
 }
